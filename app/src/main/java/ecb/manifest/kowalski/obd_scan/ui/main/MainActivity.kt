fb39f3058e37e4594695c8a88587c2bafa8698bf
Kaue Miziara <kauemiziara@gmail.com>
2023-08-24 11:50:24 -0300

    Setup bindings connecting the Main View to its ViewModel

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import ecb.manifest.kowalski.obd_scan.R
+import ecb.manifest.kowalski.obd_scan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
     }
