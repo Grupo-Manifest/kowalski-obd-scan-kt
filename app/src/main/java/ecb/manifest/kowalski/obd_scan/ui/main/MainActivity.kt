8a15d1b46b2c903d65fd178622103e6a1ec79bcb
Kaue Miziara <kauemiziara@gmail.com>
2023-08-24 20:24:24 -0300

    Implement switching tabs functionality

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivity.kt
@@ -3,18 +3,56 @@ package ecb.manifest.kowalski.obd_scan.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import ecb.manifest.kowalski.obd_scan.R
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import ecb.manifest.kowalski.obd_scan.databinding.ActivityMainBinding
+import ecb.manifest.kowalski.obd_scan.ui.adapters.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainActivityViewModel
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+
+    private lateinit var adapter: ViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        tabLayout = binding.tabLayout
+        viewPager2 = binding.viewPager
+
+        adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
+        viewPager2.adapter = adapter
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // TODO: Improve method
+                if (tab != null) viewPager2.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // TODO: Improve method
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // TODO: Improve method
+
+            }
+
+        })
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
     }
 }
