3324900729133f5fef9d7155a0cf386502d9a200
Kaue Miziara <kauemiziara@gmail.com>
2023-08-24 11:41:19 -0300

    Initial MVVM structure setup

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivity.kt
@@ -1,11 +1,17 @@
-package ecb.manifest.kowalski.obd_scan
+package ecb.manifest.kowalski.obd_scan.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import ecb.manifest.kowalski.obd_scan.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
     }
 }
---
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivityViewModel.kt
@@ -0,0 +1,5 @@
+package ecb.manifest.kowalski.obd_scan.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() { }
