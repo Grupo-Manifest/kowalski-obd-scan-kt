aa986f82cbffa2e38c530743bab981aad0c89cbf
Kaue Miziara <kauemiziara@gmail.com>
2023-08-24 22:10:10 -0300

    Implement basic bluetooth connection

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivity.kt
@@ -1,7 +1,18 @@
 package ecb.manifest.kowalski.obd_scan.ui.main
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.os.Build
+import android.os.Build.VERSION
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
@@ -17,6 +28,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var adapter: ViewPagerAdapter
 
+    private var bluetoothPermission: Boolean = false
+
+    // TODO: Proper modularization
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -36,16 +50,8 @@ class MainActivity : AppCompatActivity() {
                 // TODO: Improve method
                 if (tab != null) viewPager2.currentItem = tab.position
             }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                // TODO: Improve method
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                // TODO: Improve method
-
-            }
-
+            override fun onTabUnselected(tab: TabLayout.Tab?) { /* TODO: Implement method */ }
+            override fun onTabReselected(tab: TabLayout.Tab?) { /* TODO: Implement method */ }
         })
 
         viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
@@ -54,5 +60,60 @@ class MainActivity : AppCompatActivity() {
                 tabLayout.selectTab(tabLayout.getTabAt(position))
             }
         })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        scanBluetooth(binding.root)
+    }
+
+    // TODO
+    private fun scanBluetooth(view: View) {
+        val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Device doesn't support Bluetooth", Toast.LENGTH_LONG)
+                .show()
+        } else {
+            if (VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                bluetoothPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT)
+            } else {
+                bluetoothPermissionLauncher.launch(Manifest.permission.BLUETOOTH_ADMIN)
+            }
+        }
+    }
+
+    // TODO
+    private val bluetoothPermissionLauncher = registerForActivityResult(
+        RequestPermission()
+    ) { isGranted: Boolean ->
+        val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+
+        if (isGranted) {
+            bluetoothPermission = true
+            if (bluetoothAdapter?.isEnabled == false) {
+                val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                bluetoothActivityResultLauncher.launch(enableBluetoothIntent)
+            } else {
+                bluetoothScan()
+            }
+        } else {
+            bluetoothPermission = false
+        }
+    }
+
+    private val bluetoothActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == RESULT_OK) {
+            bluetoothScan()
+        }
+    }
+
+    private fun bluetoothScan() {
+        Toast.makeText(this, "Bluetooth connected successfully", Toast.LENGTH_LONG)
+            .show()
     }
 }
