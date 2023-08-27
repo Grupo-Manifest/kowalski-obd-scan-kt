5f9e830c758f36ace548b14f283c1249a2284e46
Kaue Miziara <kauemiziara@gmail.com>
2023-08-27 12:58:09 -0300

    Create FuelPage Composable function

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/obd_pages/fuel_page/FuelFragment.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/obd_pages/fuel_page/FuelFragment.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import ecb.manifest.kowalski.obd_scan.R
 
 class FuelFragment : Fragment() {
@@ -13,5 +21,17 @@ class FuelFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_fuel, container, false)
+    }
+}
+
+@Preview
+@Composable
+fun FuelPage() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "Fuel Page Component Placeholder")
     }
 }
