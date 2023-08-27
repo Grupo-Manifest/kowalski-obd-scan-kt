3cbfe31d45324282272bf1ad4aba2dcbf75163c6
Kaue Miziara <kauemiziara@gmail.com>
2023-08-27 13:00:33 -0300

    Create StatusPage Composable function

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/obd_pages/status_page/StatusFragment.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/obd_pages/status_page/StatusFragment.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import ecb.manifest.kowalski.obd_scan.R
 
 class StatusFragment : Fragment() {
@@ -13,5 +21,17 @@ class StatusFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_status, container, false)
+    }
+}
+
+@Preview
+@Composable
+fun StatusPage() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "Status Page Component Placeholder")
     }
 }
