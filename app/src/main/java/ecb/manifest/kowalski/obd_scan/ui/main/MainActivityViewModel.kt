3eb8c969a155ed78d74114cec07fed026470be65
Kaue Miziara <kauemiziara@gmail.com>
2023-08-27 19:17:28 -0300

    Add BluetoothUiState class to store a paired and scanned devices list && Add a state checker in the MainViewModel

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivityViewModel.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/main/MainActivityViewModel.kt
@@ -1,5 +1,29 @@
 package ecb.manifest.kowalski.obd_scan.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ecb.manifest.kowalski.obd_scan.bluetooth.IBluetoothController
+import ecb.manifest.kowalski.obd_scan.ui.presentation.BluetoothUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
 
-class MainActivityViewModel : ViewModel() { }
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    private val bluetoothController: IBluetoothController
+) : ViewModel() {
+    private val _state = MutableStateFlow(BluetoothUiState())
+    val state = combine(
+        bluetoothController.scannedDevices,
+        bluetoothController.pairedDevices,
+        _state
+    ) { scannedDevices, pairedDevices, state ->
+        state.copy(
+            scannedDevices = scannedDevices,
+            pairedDevices = pairedDevices,
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), _state.value)
+}
