7ed704a956561bd50f48c79e96c5a1fd267a4117
Kaue Miziara <kauemiziara@gmail.com>
2023-08-28 18:03:04 -0300

    Add errors SharedFlow property to IBluetoothController

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/bluetooth/IBluetoothController.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/bluetooth/IBluetoothController.kt
@@ -1,12 +1,14 @@
 package ecb.manifest.kowalski.obd_scan.bluetooth
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 
 interface IBluetoothController {
     val isConnected: StateFlow<Boolean>
     val scannedDevices: StateFlow<List<BluetoothDeviceDomain>>
     val pairedDevices: StateFlow<List<BluetoothDeviceDomain>>
+    val errors: SharedFlow<String>
 
     fun startDiscovery()
     fun stopDiscovery()
