a56ba3e90fafcc4dea4d9734d0f93c86b3b4bc8d
Kaue Miziara <kauemiziara@gmail.com>
2023-08-29 01:00:03 -0300

    Refactor EngineViewModel to depend on the new BluetoothController class

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/presentation/engine_page/EngineViewModel.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/presentation/engine_page/EngineViewModel.kt
@@ -1,30 +1,23 @@
 package ecb.manifest.kowalski.obd_scan.ui.presentation.engine_page
 
-import androidx.activity.ComponentActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ecb.manifest.kowalski.obd_scan.bluetooth.BluetoothHelper
+import ecb.manifest.kowalski.obd_scan.data.bluetooth.BluetoothController
 import ecb.manifest.kowalski.obd_scan.obd.ObdManager
 
-class EngineViewModel(private val activity: ComponentActivity) : ViewModel() {
+class EngineViewModel(private val bluetoothController: BluetoothController) : ViewModel() {
     val rpmData: MutableLiveData<String> by lazy {
         MutableLiveData<String>()
     }
 
     fun fetchData() {
-        val bluetoothHelper = BluetoothHelper(activity, activity)
         val obdManager = ObdManager()
 
-        val device = bluetoothHelper.getPairedDevices()?.first()
-
-        val bluetoothSocket = device?.let { bluetoothHelper.connectToDevice(it) }
-        if (bluetoothSocket != null) {
-            val rpmResult = obdManager.getRpm(bluetoothSocket)
+        if (bluetoothController.isConnected.value) {
+            val rpmResult = bluetoothController.bluetoothSocket?.let { obdManager.getRpm(it) }
             rpmData.value = rpmResult
-
-            bluetoothSocket.close()
         } else {
-            rpmData.value = "Null"
+            rpmData.value = "Not connected"
         }
     }
 }
