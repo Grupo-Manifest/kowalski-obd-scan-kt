57756c03163447cc2667d9c233050544ca8a0262
Kaue Miziara <kauemiziara@gmail.com>
2023-08-31 11:50:59 -0300

    Add OBD fuel data to FuelPage

--- app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/presentation/fuel_page/FuelPage.kt
+++ app/src/main/java/ecb/manifest/kowalski/obd_scan/ui/presentation/fuel_page/FuelPage.kt
@@ -5,19 +5,40 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import ecb.manifest.kowalski.obd_scan.ui.viewModels.obd.FuelViewModel
+import kotlinx.coroutines.delay
 
 @Composable
 fun FuelPage(
     viewModel: FuelViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
+    LaunchedEffect(true) {
+        viewModel.fetchData()
+    }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            viewModel.fetchData()
+            delay(2500)
+        }
+    }
+
+    val fuelLevelValue = viewModel.fuelLevelData.observeAsState().value
+    val fuelConsumptionRateValue = viewModel.fuelConsumptionRateData.observeAsState().value
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(text = "Fuel Page Component Placeholder")
+
+        Text(text = "Fuel Level: $fuelLevelValue")
+
+        Text(text = "Fuel Consumption Rate: $fuelConsumptionRateValue")
     }
 }
